03136fed9f33f57f52d9b1c4faf58683f0e618e4
NS <nikit.suvorov@gmail.com>
2022-12-15 15:37:56 +0300

    content

---
+++ components/Content/Content.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from 'next/link';
+
+const Content = () => {
+    return (<section className="c-section c-section--base">
+        <div className="container">
+            <div className="c-section__inner">
+                <div className="c-section__content">
+                    <p className="c-section__subtitle">Добро пожаловать в</p>
+                    <h1 className="c-section__title">Наш ресторан</h1>
+                    <div className="c-section__lines">
+                        <div className="c-section__line"></div>
+                        <p className="c-section__slogan">ДОМ ЛУЧШЕЙ ЕДЫ</p>
+                        <div className="c-section__line"></div>
+                    </div>
+                    <div className="c-section__action">
+                        <Link className="c-button c-button--base  c-button--base--padding" href="">VIEW MENU</Link>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>)
+}
+
+export default Content;
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import Layout from '../components/Layout/Layout';
+import Content from "../components/Content/Content";
 
 import '../styles/index.scss';
 
 
 const MyApp = ({Component, pageProps}) => {
-    return (
-        <Layout>
-            <main className="main">
-                <section className="c-section c-section--bg"></section>
-                <Component {...pageProps} />
-            </main>
-        </Layout>
-    );
+    return (<Layout>
+        <main className="main">
+            <section className="c-section c-section--bg"></section>
+            <Content/>
+            <Component {...pageProps} />
+        </main>
+    </Layout>);
 };
 
 export default MyApp;
